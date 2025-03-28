8796f00cd40693062b94b00dfffe95b9d44ceb4b
codesby-raviteja <raviteja555@gmail.com>
2025-03-28 21:40:30 +0530

    d

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client"
 import "./index.css"
 import App from "./App.jsx"
 import { Provider } from "react-redux"
-import { Store } from "./store/store.js"
+import { Store } from "./store/store.jsx"
 
 const router = createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
